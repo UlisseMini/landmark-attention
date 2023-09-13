f0850ace62695fd0ca9e62088415244df4cc99da
Ulisse Mini <ulisse.mini@gmail.com>
2023-09-13 13:16:08 +0900

    WIP (first working code) training on key retrieval

--- train.py
+++ train.py
@@ -8,51 +8,126 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 from tqdm import tqdm
 from model import PythiaLandmark, tokenize
+from typing import List
+from functools import partial
+import torch.nn.functional as F
+
+torch.random.manual_seed(0)
+
+# Train on algorithmic datase. Full plan:
+#   - existing dataset, tokenize and add landmarks manually. eq. spacing.
+#   - slice resulting logits to grab predictions. do cross entropy on correct ones,
+#     correct values can be obtained by the randperm indexing we use to create the test examples
+#   - create attention mask manually (shouldn't be that hard)
+#      - set attention mask to exclude information movement between testcases (unnecessary, but could be added)
 
 # %%
+# Hyperparameters
 
 MODEL_NAME = 'EleutherAI/pythia-14m'
 
+assert 'pythia' in MODEL_NAME, 'only pythia tokenizer has the first 128 tokens as ascii'
+token_ids = torch.arange(2, 96)
+landmark_every = 4 # how many key -> val pairs for each landmark
+window_len = 4 * landmark_every
+n_pairs = 4*window_len # multiple of window_len required for now. easier to code
+
+# Generate dataset
+
+# %%
+
+# TODO: some of this boilerplate should go in model.py
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 base_model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
 tokenizer.pad_token_id = tokenizer.eos_token_id
 
-window_len = 128
 base_model.config.update({"max_position_embeddings": window_len})
 tokenizer.add_special_tokens({'sep_token': '[LANDMARK]'})
 model = PythiaLandmark(model=base_model)
-
-# %%
-# Train on algorithmic dataset
-
-import random
-random.seed(42)
-
-N = 100
-train_labels, test_labels = [random.randint(0, N*10) for i in range(N)], [random.randint(0, N*10) for i in range(N)]
-dataset_texts = [f"KEY: {i} VALUE: {train_labels[i]}" for i in range(N)]
-test_set = [f"KEY: {i} VALUE: {test_labels[i]}" for i in range(N)]
-
-# task: context of [KEY: <tok> VAL: <tok>] plus few-shot of [KEY: <tok> VAL: <tok>] that repeat,
-# I'd like the model to learn to pay attention to previous landmarks and predict the next value
-# or... I could just train on wikipedia and interpret the logits...
-
-tokenized_train = tokenize(dataset_texts)
-tokenized_test = tokenize(test_set)
-
-# optimizer was attached to a dead model ;/
+tokenize = partial(tokenize, window_len=window_len, tokenizer=tokenizer)
 optim = torch.optim.Adam(model.parameters(), lr=1e-5)
 
-for i in tqdm(range(1000)):
-    optim.zero_grad()
-    output = model(**tokenized_train, labels=tokenized_train['input_ids'])
-    # TODO: Fix dataset so next-token prediction is right
-    # theoretical entropy rn is log(100) = 4.6 nats per VALUE prediction, since we average over
-    loss = output.loss
-    loss.backward()
-    optim.step()
-    if i % 10 == 0:
-        test_loss = model(**tokenized_test, labels=tokenized_test['input_ids']).loss
-        # TODO: accuracy
-        tqdm.write(f'loss: {loss} test_loss: {test_loss}')
+# %%
 
+kv_sep_id, line_sep_id = tokenizer.encode('-->')[0], tokenizer.encode('\n\n')[0]
+
+# TODO: Vectorize this, make batched.
+def generate_dataset():
+    # replacement would add conflicting kv pairs to training
+    keys_idx = token_ids.float().multinomial(n_pairs, replacement=False)
+    vals_idx = token_ids.float().multinomial(n_pairs, replacement=False)
+
+    # create context as key -> value\n\n
+    # TODO: Maybe this should be vectorized.
+    context = []
+    for i in range(n_pairs):
+        context += [token_ids[keys_idx[i]], kv_sep_id, token_ids[vals_idx[i]]]
+        context.append(tokenizer.sep_token_id if (i+1) % landmark_every == 0 else line_sep_id)
+
+    # TODO: maybe it'd be good to add something here other than just a landmark
+    # (a landmark is added because n_pairs % landmark_every = 0)
+    assert context[-1] == tokenizer.sep_token_id
+    testing_start = len(context)
+
+    # add testing content for retrevial
+    kv_perm = torch.randperm(n_pairs)
+    for i in range(n_pairs):
+        k = kv_perm[i]
+        context += [token_ids[keys_idx[k]], kv_sep_id, token_ids[vals_idx[k]]]
+        context.append(tokenizer.sep_token_id if (i+1) % landmark_every == 0 else line_sep_id)
+
+    # add padding
+    # edgecase: if len(context) % window_len == 0 and (n_pairs) % window_len != 0
+    # then we have uglyness and need to pad extra before adding sep_token at the end.
+    context += [tokenizer.pad_token_id] * (window_len - len(context) % window_len)
+    context[-1] = tokenizer.sep_token_id
+    context = torch.tensor([context])
+
+    # make sure everything's right
+    assert context.size(-1) % window_len == 0
+    assert (context[:, 2::4][:, :vals_idx.size(0)] == token_ids[vals_idx]).all()
+    assert (context[:, ::4][:, :keys_idx.size(0)] == token_ids[keys_idx]).all()
+    assert (context[:, window_len-1::window_len] == tokenizer.sep_token_id).all()
+
+    # Grab out the keys & values we're testing after the landmark
+    # TODO: Fix shit variable names. causing me more mental ram usage than needed
+    context_key_idx = torch.arange(testing_start, context.size(-1), 4)[:n_pairs]
+    context_val_idx = context_key_idx + 2
+
+    assert (token_ids[vals_idx[kv_perm]] == context[:, context_val_idx]).all()
+    assert (token_ids[keys_idx[kv_perm]] == context[:, context_key_idx]).all()
+
+    return (context, context_val_idx, vals_idx[kv_perm])
+
+
+# %%
+
+batch_size = 20
+
+for i in tqdm(range(1000)):
+    if (i+1) % batch_size == 0:
+        optim.step()
+        optim.zero_grad()
+    
+    context, ctx_val_idx, val_labels = generate_dataset()
+
+    # run model on trainctx + trainpred and extract logits for values
+    # could just zero out logits for the token before ->, that would remove unpredictable / essential randomness...
+    # I could also just train on ess randomness and see what happens. I need to track accuracy though...
+    # TODO: Attention mask
+    output = model(context)
+
+    # model output shifts everything right by one bc we're predicting.
+    # NOTE: loss should *only reflect prediction of values from keys. nothing else*
+    logprobs = torch.log_softmax(output.logits, -1)
+    token_loss = -logprobs[:, :-1, :].gather(dim=-1, index=context[:, 1:, None])[..., 0]
+    # so token_loss[0] is the loss predicting context[1] from context[0]
+    # therefor normal indexing we need to subtract one. maybe adding a zero would be cleaner?
+    loss = token_loss[:, ctx_val_idx-1].mean()
+    loss.backward()
+
+    if i % 10 == 0:
+        val_predictions = torch.argmax(output.logits[:, ctx_val_idx-1], -1)
+        accuracy = (val_predictions == token_ids[None, val_labels]).float().mean()
+
+        tqdm.write(f'loss: {loss} accuracy {accuracy}')
